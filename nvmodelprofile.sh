2bfe80bb95bbca7c3b21997dce57710ae030d617
sashavor <alexandra.vorobyova@gmail.com>
2023-05-25 10:42:40 -0400

    removing time constraint

--- nvmodelprofile.sh
+++ nvmodelprofile.sh
@@ -3,4 +3,4 @@
 #
 # Log for 60 seconds into output.log
 #
-nvidia-smi --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -l 1 -f "output.log"  -t 60
+nvidia-smi --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -l 1 -f "output.log"  
